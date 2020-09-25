87467e6f195e677a54ff06cb0597e598702b2bb3
Yurii <vasilvasilovichvasil@gmail.com>
2020-09-25 11:20:34 +0300

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+    return(
+        <nav className='navigation'>
+            <a>Profile</a>
+            <a>Messages</a>
+            <a>News</a>
+            <a>Music</a>
+        </nav>
+    );
+}
+export default Navbar;
